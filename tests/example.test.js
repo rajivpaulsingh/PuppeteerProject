70ba588560a036b9cb355a94d4d017fe444c9c89
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-27 17:15:05 -0700

    Code to run the test with dev tools opened

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer')
 
 describe('My First Puppetter Test', () => {
     it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({ headless: true, slowMo: 50 })
+        const browser = await puppeteer.launch({ 
+            headless: false, 
+            slowMo: 200,
+            devtools: true,
+        })
         const page = await browser.newPage()
         await page.goto('https://example.com/')
         await page.waitForSelector('h1')
