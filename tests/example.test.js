41da6a6c87dae861b0a2445dfc60f2149fa20192
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-27 17:32:40 -0700

    Code to interact with the input fields

--- tests/example.test.js
+++ tests/example.test.js
@@ -8,15 +8,21 @@ describe('My First Puppetter Test', () => {
             devtools: false,
         })
         const page = await browser.newPage()
-        await page.goto('https://example.com/')
-        await page.waitFor(2000) //implicit wait
-        await page.waitForSelector('h1')
-        await page.goto('https://dev.to/')
-        await page.waitForSelector('#top-bar')
-        await page.goBack()
-        await page.waitForSelector('h1')
-        await page.goForward()
-        await page.waitForSelector('#top-bar')
+
+        // await page.goto('https://example.com/')
+        // await page.waitFor(1000) //implicit wait
+        // await page.waitForSelector('h1')
+        // await page.goto('https://dev.to/')
+        // await page.waitForSelector('#top-bar')
+        // await page.goBack()
+        // await page.waitForSelector('h1')
+        // await page.goForward()
+        // await page.waitForSelector('#top-bar')
+        // await browser.close()
+
+        await page.goto('https://devexpress.github.io/testcafe/example/')
+        await page.type('#developer-name', 'Rajiv', { delay: 500 }) //Added delay to type is slowly ~ 500 miliseconds
         await browser.close()
+
     })
 })
