53c0894daf8926d9a6adff64992972cd120926bb
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-27 17:12:44 -0700

    Code to run the test in headless mode

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 
 describe('My First Puppetter Test', () => {
     it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({ headless: false, slowMo: 500 })
+        const browser = await puppeteer.launch({ headless: true, slowMo: 50 })
         const page = await browser.newPage()
         await page.goto('https://example.com/')
         await page.waitForSelector('h1')
