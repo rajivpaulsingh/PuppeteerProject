717b37570629902862e12006b4aed068033a6a08
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-27 17:17:10 -0700

    Code to run with implicit wait

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,11 +4,12 @@ describe('My First Puppetter Test', () => {
     it('should launch the browser', async function() {
         const browser = await puppeteer.launch({ 
             headless: false, 
-            slowMo: 200,
-            devtools: true,
+            slowMo: 10,
+            devtools: false,
         })
         const page = await browser.newPage()
         await page.goto('https://example.com/')
+        await page.waitFor(2000) //implicit wait
         await page.waitForSelector('h1')
         await browser.close()
     })
