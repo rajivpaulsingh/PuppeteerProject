2919bbd1918bff598a9e9a24bff947c0688e7834
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-28 10:46:44 -0700

    Code to interact with drop-down

--- tests/example.test.js
+++ tests/example.test.js
@@ -21,8 +21,15 @@ describe('My First Puppetter Test', () => {
         // await browser.close()
 
         await page.goto('https://devexpress.github.io/testcafe/example/')
+
+        //Input fields
         await page.type('#developer-name', 'Rajiv', { delay: 200 }) //Added delay to type is slowly ~ 200 milliseconds
+
+        //Checkbox
         await page.click('#tried-test-cafe', { clickCount: 4 }) //the clickCount is how many times it can click
+
+        //Drop-down
+        await page.select('#preferred-interface', 'JavaScript API')
         await browser.close()
 
     })
