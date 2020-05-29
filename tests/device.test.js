94515add50a166c07fef86e276354a58f7a7287a
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-29 11:19:58 -0700

    Code for Devices emulations - desktop, tablet and mobile

---
+++ tests/device.test.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer')
+
+describe('Device Emulation', () => {
+
+    let browser
+    let page
+
+    before(async function() {
+        browser = await puppeteer.launch({
+            headless: false, 
+            slowMo: 10,
+            devtools: false,
+        })
+        page = await browser.newPage()
+        await page.setDefaultTimeout(10000)
+        await page.setDefaultNavigationTimeout(20000)
+    })
+
+    after(async function() {
+        await browser.close()
+    })
+
+    it('Desktop Device Test', async function() {
+        await page.setViewport( { width: 1650, height: 1050 })
+        await page.goto('https://example.com/')
+        await page.waitFor(2000)
+    })
+
+    it('Tablet Device Test', async function() {
+        const tablet = puppeteer.devices['iPad landscape']
+        await page.emulate(tablet)
+        await page.goto('https://example.com/')
+        await page.waitFor(2000)
+    })
+
+    it('Mobile Device Test', async function() {
+        const mobile = puppeteer.devices['iPhone X']
+        await page.emulate(mobile)
+        await page.goto('https://example.com/')
+        await page.waitFor(2000)
+    })
+
+})
